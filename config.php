720df69c76c097d8a164d338dca4f5d29f5384b5
Sudheer Bulusu <bds15@iitbbs.ac.in>
2020-11-02 01:03:59 +0530

    second

--- config.php
+++ config.php
@@ -1,7 +1,7 @@
 <?php
- 	$servername = "localhost";
-    $username = "root";
-    $password = "";
-    $database = "gymkhana";
+ 	$servername = "sql12.freemysqlhosting.net";
+    $username = "sql12373882";
+    $password = "IRgx2a5BGF";
+    $database = "sql12373882";
 
 ?>
